72f664f532c5fcea4f8e9087f9a5bba84b0be81d
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-03 22:45:25 +0100

    test: test the combination of data header with skipHeader

--- src/parsers/dsv.test.js
+++ src/parsers/dsv.test.js
@@ -168,6 +168,56 @@ test('parses a dsv file with provided header and skipHeader', () => {
   )
 })
 
+test('parses a dsv file without provided header and skipHeader', () => {
+  const err                 = []
+
+  const argv                = {verbose: 0}
+  const lines               = anything()
+
+  const jsonsTokensDefaults = (
+    oneof(...delimiters).chain(delimiter =>
+      oneof(...quoteOrEscape).chain(quote =>
+        oneof(...quoteOrEscape).chain(escape =>
+          unicodeStringJsonObjectList([delimiter, quote, escape]).map(jsons => {
+            const _jsons  = jsons.map(json => Object.values(json))
+            const tokens = (
+              [Object.keys(jsons[0]).join(delimiter)]
+              .concat(jsons.map(json => Object.values(json).join(delimiter)))
+            )
+
+            return {
+              jsons: _jsons,
+              tokens,
+              defaults: {
+                delimiter,
+                quote,
+                escape,
+                header:          '[]',
+                skipHeader:      true,
+                fixedLength:     false,
+                trimWhitespaces: false,
+                skipEmptyValues: false,
+                missingIsNull:   false,
+                emptyIsNull:     false
+              }
+            }
+          })
+        )
+      )
+    )
+  )
+  
+  assert(
+    property(lines, jsonsTokensDefaults, (lines, {jsons, tokens, defaults}) =>
+      expect(
+        parserFactory(defaults)(argv)(tokens, lines)
+      ).toStrictEqual(
+        {err, jsons}
+      )
+    )
+  )
+})
+
 function unicodeStringJsonObjectList (blacklist) {
   return integer(1, 20).chain(len =>
     array(base64(), len, len).chain(keys => {
