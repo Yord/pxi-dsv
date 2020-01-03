c20a11b8516e288f6da7485d5308bb8e75d17377
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-03 22:44:00 +0100

    refactor: move helper functions to bottom

--- src/parsers/dsv.test.js
+++ src/parsers/dsv.test.js
@@ -1,42 +1,10 @@
 const {anything, array, assert, base64, constant, integer, oneof, property, unicodeString} = require('fast-check')
 const {dsv: parserFactory} = require('./dsv')
 
-function skipChars (blacklist) {
-  return string => {
-    let str = ''
-    for (let at = 0; at < string.length; at++) {
-      const ch = string[at]
-      if (blacklist.indexOf(ch) === -1) str += ch
-    }
-    return str || ' '
-  }
-}
-
-function unicodeStringJsonObjectList (blacklist) {
-  return integer(1, 20).chain(len =>
-    array(base64(), len, len).chain(keys => {
-      const _keys = keys.map(skipChars(blacklist))
-      
-      return array(array(unicodeString(1, 20), len, len), 1, 20).map(valuesList =>
-        valuesList
-        .map(values => {
-          const _values = values.map(skipChars(blacklist))
-          
-          return (
-            _keys
-            .map((key, i) => ({[key]: _values[i]}))
-            .reduce((acc, json) => Object.assign(acc, json), {})
-          )
-        })
-      )
-    })
-  )
-}
-
 const delimiters    = [',', ';', '.', '|', '/', '-', '+', '$', '#', '!'].map(constant)
 const quoteOrEscape = ["'", '"', '`', '\\'].map(constant)
 
-test('parses a dsv file without provided header and all flags false', () => {
+test('parses a dsv file without provided header', () => {
   const err                 = []
 
   const argv                = {verbose: 0}
@@ -85,7 +53,7 @@ test('parses a dsv file without provided header and all flags false', () => {
   )
 })
 
-test('parses a dsv file with provided header and all flags false', () => {
+test('parses a dsv file with provided header', () => {
   const err                 = []
 
   const argv                = {verbose: 0}
@@ -144,3 +112,35 @@ test('parses a dsv file with provided header and all flags false', () => {
     )
   )
 })
+
+function unicodeStringJsonObjectList (blacklist) {
+  return integer(1, 20).chain(len =>
+    array(base64(), len, len).chain(keys => {
+      const _keys = keys.map(skipChars(blacklist))
+      
+      return array(array(unicodeString(1, 20), len, len), 1, 20).map(valuesList =>
+        valuesList
+        .map(values => {
+          const _values = values.map(skipChars(blacklist))
+          
+          return (
+            _keys
+            .map((key, i) => ({[key]: _values[i]}))
+            .reduce((acc, json) => Object.assign(acc, json), {})
+          )
+        })
+      )
+    })
+  )
+}
+
+function skipChars (blacklist) {
+  return string => {
+    let str = ''
+    for (let at = 0; at < string.length; at++) {
+      const ch = string[at]
+      if (blacklist.indexOf(ch) === -1) str += ch
+    }
+    return str || ' '
+  }
+}
