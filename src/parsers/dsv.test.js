7b53aa7aab5ba424a4cbcd5b4935c32db9fb69f3
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-04 22:17:22 +0100

    test: add a test for error reporting of fixed length postprocessing

--- src/parsers/dsv.test.js
+++ src/parsers/dsv.test.js
@@ -232,6 +232,71 @@ test('parses a dsv file without provided header and skipHeader', () => {
   )
 })
 
+test('parses a dsv file with variable values lengths and the fixed length option', () => {
+  const argv  = {verbose: 0}
+  const lines = anything()
+
+  const jsonsTokensDefaultsErr = (
+    oneof(...delimiters).chain(delimiter =>
+      oneof(...quoteOrEscape).chain(quote =>
+        oneof(...quoteOrEscape).chain(escape =>
+          unicodeStringJsonObjectListFixedLength([delimiter, quote, escape]).chain(jsons => 
+            integer(0, jsons.length - 1).map(noOfDeletes => {
+              const tokens = noOfDeletes === 0 ? (
+                [Object.keys(jsons[0]).join(delimiter)]
+                .concat(jsons.map(json => Object.values(json).join(delimiter)))
+              ) : (
+                [Object.keys(jsons[0]).join(delimiter)]
+                .concat(
+                  jsons.slice(0, noOfDeletes).map(json => Object.values(json).slice(1).join(delimiter))
+                )
+                .concat(
+                  jsons.slice(noOfDeletes).map(json => Object.values(json).join(delimiter))
+                )
+              )
+              const _jsons = jsons.slice(noOfDeletes, jsons.length)
+
+              const err = []
+              for (let i = 0; i < noOfDeletes; i++) {
+                const msg = {msg: 'Number of values does not match number of headers'}
+                err.push(msg)
+              }
+  
+              return {
+                err,
+                jsons: _jsons,
+                tokens,
+                defaults: {
+                  delimiter,
+                  quote,
+                  escape,
+                  header:          '[]',
+                  skipHeader:      false,
+                  fixedLength:     true,
+                  trimWhitespaces: false,
+                  skipEmptyValues: false,
+                  missingIsNull:   false,
+                  emptyIsNull:     false
+                }
+              }
+            })
+          )
+        )
+      )
+    )
+  )
+  
+  assert(
+    property(lines, jsonsTokensDefaultsErr, (lines, {jsons, tokens, defaults, err}) =>
+      expect(
+        parserFactory(defaults)(argv)(tokens, lines)
+      ).toStrictEqual(
+        {err, jsons}
+      )
+    )
+  )
+})
+
 function unicodeStringJsonObjectListFixedLength (blacklist) {
   return integer(1, 20).chain(len =>
     array(base64(), len, len).chain(keys => {
