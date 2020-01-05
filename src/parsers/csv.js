6347ae5f90d7abf4e8ba8488c1560c5d3bcc0c2c
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-05 12:28:35 +0100

    docs: also name -F

--- src/parsers/csv.js
+++ src/parsers/csv.js
@@ -7,7 +7,7 @@ module.exports = {
     "--delimiter is set to ',', --quote and --escape to '\"', " +
     'and the --fixed-length flag is turned on by default. ' +
     'To turn it off, csv supports the following additional options:\n\n' +
-    '--no-pfixed-length, --no-fixed-length, -F [boolean]\nTurns off --pfixed-length and --fixed-length.\n'
+    '--no-pfixed-length, --no-fixed-length, -F [boolean]\nTurns off --pfixed-length, --fixed-length, and -F.\n'
   ),
   func: argv => {
     const {noPfixedLength, noFixedLength, F} = argv
