28d62bd192c6e1748a262cae7df1ba96b9458c96
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-05 11:03:54 +0100

    feat: change csv default flags to only --fixed-length

--- src/parsers/csv.js
+++ src/parsers/csv.js
@@ -5,45 +5,26 @@ module.exports = {
   desc: (
     'is a comma-separated values parser. It is a variant of dsv, but differs in its default values: ' +
     "--delimiter is set to ',', --quote and --escape to '\"', " +
-    'and the --fixed-length, --missing-is-null, and --empty-is-null flags are turned on by default. ' +
-    'To turn them off, csv supports the following additional options:\n\n' +
-    '--no-pfixed-length, --no-fixed-length, -F [boolean]\nTurns off --pfixed-length and --fixed-length.\n\n' +
-    '--no-pmissing-is-null, --no-missing-is-null, -M [boolean]\nTurns off --pmissing-is-null and --missing-is-null.\n\n' +
-    '--no-pempty-is-null, --no-empty-is-null, -N [boolean]\nTurns off --pempty-is-null and --empty-is-null.\n'
+    'and the --fixed-length flag is turned on by default. ' +
+    'To turn it off, csv supports the following additional options:\n\n' +
+    '--no-pfixed-length, --no-fixed-length, -F [boolean]\nTurns off --pfixed-length and --fixed-length.\n'
   ),
   func: argv => {
-    const {
-      noPfixedLength,   noFixedLength,   F,
-      noPmissingIsNull, noMissingIsNull, M,
-      noPemptyIsNull,   noEmptyIsNull,   N
-    } = argv
+    const {noPfixedLength, noFixedLength, F} = argv
 
-    const fixedLength   = !(noPfixedLength   || noFixedLength   || F || false)
-    const missingIsNull = !(noPmissingIsNull || noMissingIsNull || M || false)
-    const emptyIsNull   = !(noPemptyIsNull   || noEmptyIsNull   || N || false)
+    const fixedLength = !(noPfixedLength || noFixedLength || F || false)
 
     return dsv.dsv({
       delimiter:       ',',
       quote:           '"',
       escape:          '"',
       header:          '[]',
-      skipHeader:      false,
-      fixedLength:     true,
-      trimWhitespaces: false,
-      skipEmptyValues: false,
-      missingIsNull:   true,
-      emptyIsNull:     true
+      fixedLength:     true
     })({
       ...argv,
       pfixedLength:    fixedLength,
       fixedLength:     fixedLength,
-      F:               fixedLength,
-      pmissingIsNull:  missingIsNull,
-      missingIsNull:   missingIsNull,
-      M:               missingIsNull,
-      pemptyIsNull:    emptyIsNull,
-      emptyIsNull:     emptyIsNull,
-      N:               emptyIsNull
+      F:               fixedLength
     })
   }
 }
