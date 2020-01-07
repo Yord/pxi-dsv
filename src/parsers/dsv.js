59ba4b0e20b2249ec2defd42ebfd64bc1485bb77
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-07 22:08:51 +0100

    chore: rename postProcessing to postprocessing

--- src/parsers/dsv.js
+++ src/parsers/dsv.js
@@ -75,20 +75,20 @@ function dsv (defaults) {
     let ignoreDataHeader   =  _skipHeader
     const returnTypeObject = !_skipHeader || keysLength > 0
 
-    const postProcessingFs = []
-    if (_fixedLength)     postProcessingFs.push(controlFixedLength)
-    if (_skipEmptyValues) postProcessingFs.push(removeEmptyValues)
-    if (_trimWhitespaces) postProcessingFs.push(removeWhitespaces)
-    if (_emptyAsNull)     postProcessingFs.push(emptyToNull)
-    if (_skipNull)        postProcessingFs.push(removeNulls)
-    if (_missingAsNull)   postProcessingFs.push(missingToNull)
+    const postprocessingFs = []
+    if (_fixedLength)     postprocessingFs.push(controlFixedLength)
+    if (_skipEmptyValues) postprocessingFs.push(removeEmptyValues)
+    if (_trimWhitespaces) postprocessingFs.push(removeWhitespaces)
+    if (_emptyAsNull)     postprocessingFs.push(emptyToNull)
+    if (_skipNull)        postprocessingFs.push(removeNulls)
+    if (_missingAsNull)   postprocessingFs.push(missingToNull)
 
-    const postProcessingF = (values, line) => {
+    const postprocessingF = (values, line) => {
       let err     = []
       let values2 = values
 
-      for (let i = 0; i < postProcessingFs.length; i++) {
-        const f   = postProcessingFs[i]
+      for (let i = 0; i < postprocessingFs.length; i++) {
+        const f   = postprocessingFs[i]
         const res = f(values2, line)
         if (res.err.length > 0) err = err.concat(res.err)
         values2   = res.values
@@ -168,7 +168,7 @@ function dsv (defaults) {
         }
 
         const line = verbose > 0 ? lines[i] : undefined
-        const res  = postProcessingF(values, line)
+        const res  = postprocessingF(values, line)
         if (res.err.length > 0) err = err.concat(res.err)
         values     = res.values
 
