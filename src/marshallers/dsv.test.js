fcc0b34ce71d5b14d04be979f85a18eec0115057
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-07 18:26:23 +0100

    test: add a test for handling missing options with verbose 1

--- src/marshallers/dsv.test.js
+++ src/marshallers/dsv.test.js
@@ -45,3 +45,47 @@ test('parses a dsv file with missing options and verbose 0', () => {
     )
   )
 })
+
+test('parses a dsv file with missing options and verbose 1', () => {
+  const argv                = {verbose: 1}
+
+  const err                 = [
+    {msg: 'Please provide mrecordSeparator, recordSeparator or R option', line: -1},
+    {msg: 'Please provide mdelimiter, delimiter or D option',             line: -1},
+    {msg: 'Please provide mquote, quote or Q option',                     line: -1},
+    {msg: 'Please provide mescape, escape or C option',                   line: -1},
+    {msg: 'Please provide mheader, header or H option',                   line: -1}
+  ]
+
+  const jsonsStrDefaults = (
+    boolean().chain(fixedLength =>
+      unicodeStringJsonObjectListFixedLength([]).map(jsons => {
+        const str = ''
+
+        return {
+          jsons,
+          str,
+          defaults: {
+            skipHeader:      false,
+            fixedLength,
+            trimWhitespaces: false,
+            skipEmptyValues: false,
+            missingAsNull:   false,
+            emptyAsNull:     false,
+            skipNull:        false
+          }
+        }
+      })
+    )
+  )
+  
+  assert(
+    property(jsonsStrDefaults, ({jsons, str, defaults}) =>
+      expect(
+        marshallerFactory(defaults)(argv)(jsons)
+      ).toStrictEqual(
+        {err, str}
+      )
+    )
+  )
+})
