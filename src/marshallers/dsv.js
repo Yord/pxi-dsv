7f52b3ee047a58b7f5a52dfa5282e54ac00e61a7
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-09 14:40:54 +0100

    feat: implement replace nulls

--- src/marshallers/dsv.js
+++ src/marshallers/dsv.js
@@ -273,10 +273,16 @@ function dsv (defaults) {
       return record2
     }
 
-      // use _missingAs
-      _missingAs
-
     function replaceNulls (record) {
+      if (headerIsSet) {
+        const record2 = []
+        for (let i = 0; i < keys.length; i++) {
+          const value = record[i]
+          if (value === null || typeof value === 'undefined') record2.push(_nullAs)
+          else record2.push(value)
+        }
+        return record2
+      }
       return record
     }
   }
