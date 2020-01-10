9942f689100118727dc94be44363639f332ac573
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-10 11:35:39 +0100

    docs: encode tab in usage description

--- src/deserializers/tsv.js
+++ src/deserializers/tsv.js
@@ -4,7 +4,7 @@ module.exports = {
   name: 'tsv',
   desc: (
     'is a tab-separated values deserializer. It is a variant of dsv, but differs in its default values: ' +
-    "--delimiter is set to '\t' (tab), --quote and --escape to '\"' (but should not be used in tsv files), " +
+    "--delimiter is set to '\\t' (tab), --quote and --escape to '\"' (but should not be used in tsv files), " +
     'and the --fixed-length flag is turned on by default. ' +
     'To turn it off, csv supports the following additional options:\n\n' +
     '--no-dfixed-length, --no-fixed-length, -F [boolean]\nTurns off --dfixed-length, --fixed-length, and -F.\n'
--- src/serializers/tsv.js
+++ src/serializers/tsv.js
@@ -4,7 +4,7 @@ module.exports = {
   name: 'tsv',
   desc: (
     'is a tab-separated values serializer. It is a variant of dsv, but differs in its default values: ' +
-    "--record-separator is set to '\\n', --delimiter is set to '\t' (tab), --quote and --escape to '\"' " +
+    "--record-separator is set to '\\n', --delimiter is set to '\\t' (tab), --quote and --escape to '\"' " +
     '(but should not be used in tsv files), and the --fixed-length flag is turned on by default. ' +
     'To turn it off, csv supports the following additional options:\n\n' +
     '--no-sfixed-length, --no-fixed-length, -F [boolean]\nTurns off --sfixed-length, --fixed-length, and -F.\n'
