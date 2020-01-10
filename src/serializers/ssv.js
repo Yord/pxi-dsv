66c1417371cace5d106420fbde58b11b5297c1f9
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-10 11:34:39 +0100

    fix: correct the ssv name

--- src/serializers/ssv.js
+++ src/serializers/ssv.js
@@ -3,7 +3,7 @@ const dsv = require('./dsv')
 module.exports = {
   name: 'ssv',
   desc: (
-    'is a comma-separated values serializer. It is a variant of dsv, but differs in its default values: ' +
+    'is a space-separated values serializer. It is a variant of dsv, but differs in its default values: ' +
     "--record-separator is set to '\\n', --delimiter is set to ' ' (one space), --quote and --escape to '\"', " +
     'and the --skip-header flag is turned on by default. ' +
     'To turn it off, csv supports the following additional options:\n\n' +
